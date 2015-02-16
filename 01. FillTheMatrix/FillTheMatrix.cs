1a47e1a89d31692db43183833d680ab0ce9efd3b
dimanov <dimanov@gmail.com>
2015-02-16 03:13:50 +0200

    Problem 1, Variant A added

--- 01. FillTheMatrix/FillTheMatrix.cs
+++ 01. FillTheMatrix/FillTheMatrix.cs
@@ -1,6 +1,8 @@
 ﻿/*
  * Problem 1. Fill the matrix
  * Write a program that fills and prints a matrix of size  (n, n)
+ * 
+ * 
  */
 
 using System;
@@ -9,6 +11,33 @@ class FillTheMatrix
 {
     static void Main()
     {
+        Console.Write("Please enter the size of the matrix \nn=");
+        int matrixSize = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[matrixSize, matrixSize];
 
+        // Variant A
+
+        for (int col = 0; col < matrixSize; col++)
+        {
+            for (int row = 0; row < matrixSize; row++ )
+            {
+                matrix[row, col] = col*matrixSize + (row +1);
+            }
+        }
+
+        PrintMatrix(matrix);
+
+    }
+
+    private static void PrintMatrix(int [,] array)
+    {
+        for(int row = 0; row < array.GetLength(0); row++)
+        {
+            for (int col = 0; col < array.GetLength(1); col++)
+            {
+                Console.Write("{0,3}  ", array[row,col]);
+            }
+            Console.WriteLine();
+        }
     }
 }
